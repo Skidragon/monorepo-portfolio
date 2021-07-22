9e9da6243db6169cb91fc7ce73820d7a7cb2afba
Simon Davis <simonfreelance1@gmail.com>
2021-07-22 18:33:58 -0400

    fixed imports for product-feedback-ui-components

--- libs/product-feedback/ui/src/index.ts
+++ libs/product-feedback/ui/src/index.ts
@@ -1,4 +1,1 @@
-export * from './lib/buttons/button/button';
-export * from './dropdowns/dropdown/dropdown';
-export * from './buttons/upvote-button/upvote-button';
-export * from './buttons/button/button';
+export * from './lib/buttons/button/button'
