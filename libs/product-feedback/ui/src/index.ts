1ab837042ab8bc88fa1be93412747af91134f2e8
Simon Davis <simonfreelance1@gmail.com>
2021-07-22 18:26:20 -0400

    generated storybook and got button hooked up

--- libs/product-feedback/ui/src/index.ts
+++ libs/product-feedback/ui/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/buttons/button/button';
 export * from './dropdowns/dropdown/dropdown';
 export * from './buttons/upvote-button/upvote-button';
 export * from './buttons/button/button';
