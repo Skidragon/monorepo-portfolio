12733abfcb472ec09bf716e294ded65a0fbea1a4
Simon Davis <simonfreelance1@gmail.com>
2021-07-23 13:46:18 -0400

    Removed unused feature lib

--- libs/product-feedback/feature/project/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/no-feedbacks-card/no-feedbacks-card';
-export * from './lib/project-feedback-board-card/project-feedback-board-card';
-export * from './lib/product-feedback-feature-project';
