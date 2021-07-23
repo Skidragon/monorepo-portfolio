bc6fc0f5aa55776e794ff7a4c3cc49b92440cd4f
Simon Davis <simonfreelance1@gmail.com>
2021-07-23 04:53:23 -0400

    Added more tests for button

--- libs/product-feedback/ui/src/lib/buttons/button/button.spec.tsx
+++ libs/product-feedback/ui/src/lib/buttons/button/button.spec.tsx
@@ -4,7 +4,12 @@ import Button from './button';
 
 describe('Button', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Button />);
+    const { baseElement } = render(<Button>Test</Button>);
     expect(baseElement).toBeTruthy();
   });
+  it('should have text', () => {
+    const { baseElement } = render(<Button>Test</Button>);
+    expect(baseElement.textContent).toBe('Test');
+  });
+  // it.todo('should have an icon', () => {});
 });
