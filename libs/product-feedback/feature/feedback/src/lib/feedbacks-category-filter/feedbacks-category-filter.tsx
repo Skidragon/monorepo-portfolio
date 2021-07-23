0ea149f2f5ec689ff5100853c726ee9169267f2d
Simon Davis <simonfreelance1@gmail.com>
2021-07-23 14:30:03 -0400

    Started on FeedbacksCategoryFilter

--- libs/product-feedback/feature/feedback/src/lib/feedbacks-category-filter/feedbacks-category-filter.tsx
+++ libs/product-feedback/feature/feedback/src/lib/feedbacks-category-filter/feedbacks-category-filter.tsx
@@ -1,16 +1,31 @@
+import { Box, Button } from '@sd/product-feedback-ui-components';
 import styled from 'styled-components';
 
 /* eslint-disable-next-line */
 export interface FeedbacksCategoryFilterProps {}
 
-const StyledFeedbacksCategoryFilter = styled.div`
+const StyledFeedbacksCategoryFilter = styled(Box)`
   color: pink;
 `;
 
+const Cluster = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--space, 1rem);
+  justify-content: flex-start;
+  align-items: center;
+`;
 export function FeedbacksCategoryFilter(props: FeedbacksCategoryFilterProps) {
   return (
     <StyledFeedbacksCategoryFilter>
-      <h1>Welcome to feedbacks-category-filter!</h1>
+      <Cluster>
+        <Button>All</Button>
+        <Button>UI</Button>
+        <Button>UX</Button>
+        <Button>Enahncement</Button>
+        <Button>Bug</Button>
+        <Button>Feature</Button>
+      </Cluster>
     </StyledFeedbacksCategoryFilter>
   );
 }
