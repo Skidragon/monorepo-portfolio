9a8d947c303ffdb92e3f39ce75569a11f2818c76
Simon Davis <simonfreelance1@gmail.com>
2021-07-24 16:12:55 -0400

    Created basic NoFeedbacksCard component

--- libs/product-feedback/feature/feedback/src/lib/no-feedbacks-card/no-feedbacks-card.tsx
+++ libs/product-feedback/feature/feedback/src/lib/no-feedbacks-card/no-feedbacks-card.tsx
@@ -1,16 +1,24 @@
 import styled from 'styled-components';
-
+import { Box, Button } from '@sd/product-feedback-ui-components';
 /* eslint-disable-next-line */
 export interface NoFeedbacksCardProps {}
 
-const StyledNoFeedbacksCard = styled.div`
-  color: pink;
+const StyledNoFeedbacksCard = styled(Box)`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export function NoFeedbacksCard(props: NoFeedbacksCardProps) {
   return (
     <StyledNoFeedbacksCard>
-      <h1>Welcome to no-feedbacks-card!</h1>
+      <h3>There is no feedback yet.</h3>
+      <p>
+        Got a suggestion? Found a bug that needs to be squashed? We love hearing
+        about new ideas to improve our app.
+      </p>
+      <Button variant="primary">Add Feedback</Button>
     </StyledNoFeedbacksCard>
   );
 }
