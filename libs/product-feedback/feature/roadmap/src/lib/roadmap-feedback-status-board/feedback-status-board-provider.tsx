cea194d16e5449c9a97004a3ddad3c6e22d0eafb
Simon Davis <simonfreelance1@gmail.com>
2021-08-04 13:27:49 -0400

    Fixed adding feedback to the correct position

--- libs/product-feedback/feature/roadmap/src/lib/roadmap-feedback-status-board/feedback-status-board-provider.tsx
+++ libs/product-feedback/feature/roadmap/src/lib/roadmap-feedback-status-board/feedback-status-board-provider.tsx
@@ -61,7 +61,7 @@ function addItem<T>(array: T[], item: T, insertIndex: number): T[] {
   if (insertIndex === 0) {
     return [item, ...array];
   }
-  if (insertIndex === array.length - 1) {
+  if (insertIndex === array.length) {
     return [...array, item];
   }
   return [...array.slice(0, insertIndex), item, ...array.slice(insertIndex)];
