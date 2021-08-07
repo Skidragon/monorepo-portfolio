fa4ed5113811d50d57d93841250b8c9761f04bdb
Simon Davis <simonfreelance1@gmail.com>
2021-08-07 14:35:56 -0400

    Started Grid and Stack layout components

--- libs/shared/ui/layout/src/lib/grid/grid.tsx
+++ libs/shared/ui/layout/src/lib/grid/grid.tsx
@@ -1,16 +1,40 @@
 import styled from 'styled-components';
-
+import { BaseDiv } from '@sd/react-component-types';
 /* eslint-disable-next-line */
-export interface GridProps {}
+export interface GridProps extends BaseDiv {
+  rowGap?: number;
+  columnGap?: number;
+  space?: number;
+  children: React.ReactNode;
+}
 
-const StyledGrid = styled.div`
-  color: pink;
+const StyledGrid = styled.div<
+  Pick<GridProps, 'rowGap' | 'columnGap' | 'space'>
+>`
+  display: grid;
+  column-gap: ${(props) => props.columnGap || props.space || 1}rem;
+  row-gap: ${(props) => props.rowGap || props.space || 1}rem;
+  @supports (width: min(250px, 100%)) {
+    grid-template-columns: repeat(auto-fit, minmax(min(250px, 100%), 1fr));
+  }
 `;
 
-export function Grid(props: GridProps) {
+export function Grid({
+  rowGap = 1,
+  columnGap = 1,
+  space = 1,
+  children,
+  ...props
+}: GridProps) {
   return (
-    <StyledGrid>
-      <h1>Welcome to grid!</h1>
+    <StyledGrid
+      rowGap={rowGap}
+      columnGap={columnGap}
+      space={space}
+      {...props}
+      ref={undefined}
+    >
+      {children}
     </StyledGrid>
   );
 }
--- libs/shared/ui/layout/src/lib/stack/stack.tsx
+++ libs/shared/ui/layout/src/lib/stack/stack.tsx
@@ -1,18 +1,27 @@
-import styled from 'styled-components';
-
+import styled, { css } from 'styled-components';
 /* eslint-disable-next-line */
-export interface StackProps {}
+export interface StackProps {
+  children: React.ReactNode;
+  isRecursive?: boolean;
+  space?: number;
+}
 
-const StyledStack = styled.div`
-  color: pink;
+const StyledStack = styled.div<Pick<StackProps, 'isRecursive' | 'space'>>`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  & > * {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  & > * + * {
+    margin-top: var(--flow, 1.5rem);
+  }
 `;
 
 export function Stack(props: StackProps) {
-  return (
-    <StyledStack>
-      <h1>Welcome to stack!</h1>
-    </StyledStack>
-  );
+  return <StyledStack {...props} />;
 }
 
 export default Stack;
