fb10387366ee23cac3397a922bffdc0d48136566
Simon Davis <simonfreelance1@gmail.com>
2021-08-07 13:18:37 -0400

    initial products gallery page created

--- apps/product-feedback/pages/products-gallery.tsx
+++ apps/product-feedback/pages/products-gallery.tsx
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 import {
   Button,
+  Card,
   Dropdown,
-  DropdownOption,
+  DropdownItem,
 } from '@sd/product-feedback-ui-components';
+import Link from 'next/link';
+import { useState } from 'react';
 /* eslint-disable-next-line */
 
 export interface ProductsGalleryProps {
@@ -13,6 +16,7 @@ export interface ProductsGalleryProps {
 const StyledProductsGallery = styled.div``;
 const ProductBar = styled.div`
   display: flex;
+  align-items: center;
   background: var(--color-secondary);
   padding: 1.5em;
 `;
@@ -22,24 +26,81 @@ const Title = styled.h1`
 const AddProductButton = styled(Button)`
   margin-left: auto;
 `;
+const Grid = styled.section`
+  display: grid;
+  column-gap: 2rem;
+  row-gap: 4rem;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-auto-rows: 20rem;
+  align-items: stretch;
+  margin-top: 4rem;
+`;
+const BreakoutButton = styled(Button)`
+  margin-top: 2rem;
+  width: 100%;
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
 export function ProductsGallery(props: ProductsGalleryProps) {
+  const [item, setItem] = useState<DropdownItem>({
+    value: 'public',
+    label: 'Public Products',
+  });
   return (
     <StyledProductsGallery>
       <ProductBar>
-        <Title>Product Gallery</Title>
-        <Dropdown
-          onOptionChange={(e) => {
-            console.log(e);
-          }}
-          label={'Product Scope'}
-          value={'public'}
-          isOpen={false}
-        >
-          <DropdownOption value={'public'}>Public</DropdownOption>
-          <DropdownOption value={'private'}>Private</DropdownOption>
-        </Dropdown>
+        <div>
+          <Title>Product Gallery</Title>
+          <Dropdown
+            label={'Show'}
+            selectedItem={item}
+            onSelectedItemChange={(changes) => {
+              setItem(changes.selectedItem);
+            }}
+            items={[
+              {
+                value: 'public',
+                label: 'Public Products',
+              },
+              {
+                value: 'private',
+                label: 'Private Products',
+              },
+            ]}
+          />
+        </div>
         <AddProductButton variant={'primary'}>+ Add Product</AddProductButton>
       </ProductBar>
+      <Grid>
+        <Card title={'Frontend Mentor'}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea eius
+          beatae doloremque sed eveniet non!
+          <Link href={'#'} passHref={true}>
+            <BreakoutButton variant={'secondary'}>
+              Frontend Mentor ▶️
+            </BreakoutButton>
+          </Link>
+        </Card>
+        <Card title={'Product Feedback'}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea eius
+          beatae doloremque sed eveniet non!
+          <BreakoutButton variant={'secondary'}>
+            Product Feedback ▶️
+          </BreakoutButton>
+        </Card>
+        <Card title={'Linkedin'}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea eius
+          beatae doloremque sed eveniet non!
+          <BreakoutButton variant={'secondary'}>Linkedin ▶️</BreakoutButton>
+        </Card>
+      </Grid>
     </StyledProductsGallery>
   );
 }
