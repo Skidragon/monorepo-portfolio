c712e523c412cf8bbd165b286b8a219f8824e70c
Simon Davis <simonfreelance1@gmail.com>
2021-09-08 15:14:34 -0400

    Updated reply field

--- libs/product-feedback/feature/comment/src/lib/reply-form/reply-form.tsx
+++ libs/product-feedback/feature/comment/src/lib/reply-form/reply-form.tsx
@@ -10,8 +10,20 @@ const Form = styled.form`
   display: grid;
   grid-template-columns: 1fr auto;
   grid-gap: 1rem;
+  align-items: flex-start;
 `;
-
+const Textarea = styled.textarea`
+  -webkit-appearance: none;
+  -ms-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background: #f2f2f2;
+  padding: 1em 1rem;
+  font-size: 1em;
+  border-radius: 6px;
+  width: 100%;
+  border: none;
+`;
 export function ReplyForm(props: ReplyFormProps) {
   const [reply, setReply] = useState('');
   return (
@@ -20,14 +32,13 @@ export function ReplyForm(props: ReplyFormProps) {
         e.preventDefault();
       }}
     >
-      <Input
+      <Textarea
+        rows={3}
         value={reply}
-        label={`reply to @${props.handle}`}
-        id="reply-field"
+        placeholder={`@${props.handle}`}
         onChange={(e) => {
           setReply(e.target.value);
         }}
-        placeholder="reply"
       />
       <Button variant="primary" type="submit">
         Post Reply
