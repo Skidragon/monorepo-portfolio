98608808b52ddd13739d36ef6613d784e938331d
Simon Davis <simonfreelance1@gmail.com>
2021-11-28 14:33:21 -0500

    Able to get total comments in feedback details page

--- apps/product-feedback/pages/product/[productId]/feedback/[feedbackId]/details.tsx
+++ apps/product-feedback/pages/product/[productId]/feedback/[feedbackId]/details.tsx
@@ -12,9 +12,47 @@ export interface FeedbackDetailsProps {}
 const StyledFeedbackDetails = styled(Stack)`
   padding: var(--flow);
 `;
-
+type CommentProps = {
+  firstName: string;
+  lastName: string;
+  handle: string;
+  comment: string;
+  replies: Exclude<CommentProps, 'replies'>[];
+};
+const comments: CommentProps[] = [
+  {
+    firstName: 'Elijah',
+    lastName: 'Moss',
+    handle: 'hexagon.bestagon',
+    comment:
+      'Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.',
+    replies: [],
+  },
+  {
+    firstName: 'James',
+    lastName: 'Skinner',
+    handle: 'hummingbird1',
+    comment:
+      'Second this! I do a lot of late night coding and reading. Adding a dark theme can be great for preventing eye strain and the headaches that result. It’s also quite a trend with modern apps and  apparently saves battery life.',
+    replies: [
+      {
+        firstName: 'Anne',
+        lastName: 'Valentine',
+        handle: 'annex1990',
+        comment:
+          'While waiting for dark mode, there are browser extensions that will also do the job. Search for "dark theme” followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.',
+        replies: [],
+      },
+    ],
+  },
+];
 export function FeedbackDetails(props: FeedbackDetailsProps) {
   const [category, setCategory] = useState('All');
+  const totalComments =
+    comments.length +
+    comments.reduce((acc, curr) => {
+      return acc + curr.replies.length;
+    }, 0);
   return (
     <Center>
       <StyledFeedbackDetails>
@@ -22,36 +60,10 @@ export function FeedbackDetails(props: FeedbackDetailsProps) {
           title={'Add a dark theme option'}
           description={'It would help people like me with light sensitivities.'}
           category="Enhancement"
+          upvotes={102}
+          commentsAmount={totalComments}
         />
-        <Comments
-          data={[
-            {
-              firstName: 'Elijah',
-              lastName: 'Moss',
-              handle: 'hexagon.bestagon',
-              comment:
-                'Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.',
-              replies: [],
-            },
-            {
-              firstName: 'James',
-              lastName: 'Skinner',
-              handle: 'hummingbird1',
-              comment:
-                'Second this! I do a lot of late night coding and reading. Adding a dark theme can be great for preventing eye strain and the headaches that result. It’s also quite a trend with modern apps and  apparently saves battery life.',
-              replies: [
-                {
-                  firstName: 'Anne',
-                  lastName: 'Valentine',
-                  handle: 'annex1990',
-                  comment:
-                    'While waiting for dark mode, there are browser extensions that will also do the job. Search for "dark theme” followed by your browser. There might be a need to turn off the extension for sites with naturally black backgrounds though.',
-                  replies: [],
-                },
-              ],
-            },
-          ]}
-        />
+        <Comments data={comments} />
         <CreateCommentForm />
       </StyledFeedbackDetails>
     </Center>
