0ea353bed8d80e5a07972131e7f7a328f89e291c
Simon Davis <simonfreelance1@gmail.com>
2022-02-23 20:40:53 -0500

    advice-gen project starting files

---
+++ apps/advice-gen/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'advice-gen',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/advice-gen',
+};
