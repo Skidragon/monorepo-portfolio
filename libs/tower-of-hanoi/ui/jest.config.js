857dba3c473eb0255bab69993ddfc6770b4d7f60
Simon Davis <simonfreelance1@gmail.com>
2022-02-26 18:37:25 -0500

    Generated ui lib for tower-of-hanoi

---
+++ libs/tower-of-hanoi/ui/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'tower-of-hanoi-ui',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/tower-of-hanoi/ui',
+};
