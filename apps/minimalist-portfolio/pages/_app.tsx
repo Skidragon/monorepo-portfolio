04875a2c5381a3b611a731a3fbdb5437fa1b98dd
Simon Davis <simonfreelance1@gmail.com>
2022-02-25 21:14:26 -0500

    Updated title and favicon

--- apps/minimalist-portfolio/pages/_app.tsx
+++ apps/minimalist-portfolio/pages/_app.tsx
@@ -152,7 +152,26 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to minimalist-portfolio!</title>
+        <title>Simon Davis</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <GlobalStyle />
       <div className="app">
