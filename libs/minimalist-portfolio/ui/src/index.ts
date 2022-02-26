572cec743c079b2465b7d0e1ec9d1fd902f10bc3
Simon Davis <simonfreelance1@gmail.com>
2022-02-26 17:20:29 -0500

    TextArea component created

--- libs/minimalist-portfolio/ui/src/index.ts
+++ libs/minimalist-portfolio/ui/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/link-button/link-button';
 export * from './lib/text-field/text-field';
 export * from './lib/button/button';
 export * from './lib/link-to/link-to';
+export * from './lib/text-area/text-area';
