58ad4afee8602a72d90bf375168f390259f73483
Simon Davis <simonfreelance1@gmail.com>
2022-03-05 18:32:48 -0500

    Deleted unused components

--- libs/audiophile/ui/src/lib/format-price/format-price.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import FormatPrice from './format-price';
-describe('FormatPrice', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<FormatPrice price={200} />);
-    expect(baseElement).toBeTruthy();
-  });
-});
