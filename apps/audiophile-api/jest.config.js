9bcbc7659550047dd84ef37d0cfaf12ed00bf901
Simon Davis <simonfreelance1@gmail.com>
2022-03-06 05:58:30 -0500

    able to get audiophile-api running

---
+++ apps/audiophile-api/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'audiophile-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/audiophile-api',
+};
--- apps/audiophile-api/src/app/app.controller.ts
+++ apps/audiophile-api/src/app/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,11 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-  @Get()
-  GetCategories() {
-    
+  getData() {
+    return this.appService.getData();
   }
 }
