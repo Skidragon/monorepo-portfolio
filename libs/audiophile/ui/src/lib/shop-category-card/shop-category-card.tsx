e034f07afce597db7bc18e594805ff1ea987abda
Simon Davis <simonfreelance1@gmail.com>
2022-03-05 20:24:42 -0500

    Refactored interface

--- libs/audiophile/ui/src/lib/shop-category-card/shop-category-card.tsx
+++ libs/audiophile/ui/src/lib/shop-category-card/shop-category-card.tsx
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import Link from 'next/link';
 /* eslint-disable-next-line */
-interface Category {
+
+export interface ShopCategoryCardProps {
   category: string;
   src: string;
 }
-export interface ShopCategoryCardProps extends Category {}
 
 const StyledShopCategoryCard = styled.div`
   display: flex;
