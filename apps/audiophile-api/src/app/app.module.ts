fdf546c54ac057882e231b6aba6da18e5d209517
Simon Davis <simonfreelance1@gmail.com>
2022-03-09 12:22:25 -0500

    Added throttler to protect from DOS

--- apps/audiophile-api/src/app/app.module.ts
+++ apps/audiophile-api/src/app/app.module.ts
@@ -3,9 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ThrottlerModule } from '@nestjs/throttler';
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 30,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
