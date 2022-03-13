eb44327f51cf4e96abf8d205060d0c7df1d7ddda
Simon Davis <simonfreelance1@gmail.com>
2022-03-13 15:32:28 -0400

    tested Token component

--- libs/memory/ui/src/lib/token/token.spec.tsx
+++ libs/memory/ui/src/lib/token/token.spec.tsx
@@ -4,7 +4,12 @@ import Token from './token';
 
 describe('Token', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Token />);
+    const { baseElement } = render(<Token state={'MATCH'} />);
     expect(baseElement).toBeTruthy();
   });
+  it('should display a value', () => {
+    const { getByText } = render(<Token state={'MATCH'}>3</Token>);
+    expect(getByText('3').textContent).toBe('3');
+  });
+  
 });
